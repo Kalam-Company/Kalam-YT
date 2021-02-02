0940774fc56ba47def4047ada1356ea6a795dd69
Kalam Company <77258060+Kalam-Company@users.noreply.github.com>
2021-02-02 13:17:39 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,14 +3,4 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"""Hello, It's not Difficult!!!
-
-<b>Follow these Steps...</b>
-
-⛳️ Send Custom Thumbnail if required (It will be saved permenantly!).
-
-⛳️ Send your youtube link and select desired option.
-
-⛳️ That's all, It's simple.
-
-📝 Currently Only supports Youtube Single Video (No playlist)."""
+    helptxt = f"Hello, It's not Difficult!!!\n\n<b>Follow these Steps...</b>\n\n⛳️ Send Custom Thumbnail if required (It will be saved permenantly!).\n\n⛳️ Send your youtube link and select desired option.\n\n⛳️ That's all, It's simple.\n\n📝 Currently Only supports Youtube Single Video (No playlist)."
